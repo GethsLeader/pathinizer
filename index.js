cbe80db9df96abc6db1a764fc577a1e66049972e
Timur Bagaviev <GethsLeader@gmail.com>
2016-09-21 10:48:51 +0600

    "join" function based on path.join added.

--- index.js
+++ index.js
@@ -10,7 +10,21 @@ var paths = {
     node: process.argv[0],
     script: null,
     directory: null,
-    temp: null
+    temp: null,
+    join: function (firstArgument) {
+        if (firstArgument) {
+            let result = '';
+            arguments.forEach((value)=> {
+                if (typeof value != 'string') {
+                    throw new Error('Cannot join anything instead of strings to path!');
+                }
+                result = path.join(result, value);
+            });
+            return result;
+        } else {
+            throw new Error('Cannot join nothing to path!');
+        }
+    }
 };
 
 paths.script = process.argv[1];
